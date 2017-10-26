75beaed0690518143395e7c6697fc736c9ba9446
BaronNox <baron.von.stabossen@gmail.com>
2017-10-26 16:15:21 +0200

    -TabPane in Constructor

--- SOTAC's Marketer/src/application/tabs/TabGeneric.java
+++ SOTAC's Marketer/src/application/tabs/TabGeneric.java
@@ -7,17 +7,10 @@ public class TabGeneric extends Tab
 {
 	private static int tabID = 0;
 	
-	private TabPane tabPane;
-	
-	public TabGeneric(TabPane tabPane)
+	public TabGeneric()
 	{
 		setId(String.valueOf(getNextID()));
-		this.tabPane = tabPane;
 		setClosable(false);
-		setOnSelectionChanged(e -> {
-			
-		});
-		
 	}
 	
 	public static int getNextID() {
