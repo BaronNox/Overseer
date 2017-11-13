9e91d441e270eed97bd57c22a7080a5e509d8568
BaronNox <baron.von.stabossen@gmail.com>
2017-11-13 14:54:26 +0100

    +DescendingPriceComperator for sorting Sell Orders

---
+++ SOTAC's Marketer/src/net/noxumbrarum/sotacmarketer/comperator/DescendingPriceComperator.java
@@ -0,0 +1,22 @@
+package net.noxumbrarum.sotacmarketer.comperator;
+
+import java.util.Comparator;
+
+import net.noxumbrarum.sotacmarketer.data.MarketOrder;
+
+public class DescendingPriceComperator implements Comparator<MarketOrder>
+{
+
+	@Override
+	public int compare(MarketOrder o1, MarketOrder o2)
+	{
+		if(o1.getPrice() == o2.getPrice()) {
+			return 0;
+		} else if (o1.getPrice() < o2.getPrice()) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+}
